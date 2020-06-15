a3a886d0f3aa92a74f986e064f5ecc5ff4258d68
Alexandre Amory <amamory@gmail.com>
2020-06-14 21:05:38 -0300

    getting design name from an environment variable

--- Vivado/build_bitstream_export_sdk.tcl
+++ Vivado/build_bitstream_export_sdk.tcl
@@ -6,15 +6,14 @@ set design_name $::env(VIVADO_DESIGN_NAME)
 puts "Using design name: ${design_name}"
 
 # generate bitstream
-#set project_file [glob -directory . *.xpr]
-open_project ./$design_name/$design_name.xpr
+open_project ./${design_name}/${design_name}.xpr
 update_compile_order -fileset sources_1
 launch_runs impl_1 -to_step write_bitstream -jobs 8
 wait_on_run impl_1
 
 # exporting hw design to SDK
-file mkdir ./$design_name/zedboard_base.sdk
-file copy -force ./$design_name/zedboard_base.runs/impl_1/zedboard_base_wrapper.sysdef ./$design_name/zedboard_base.sdk/zedboard_base_wrapper.hdf
+file mkdir ./${design_name}/${design_name}.sdk
+file copy -force ./${design_name}/${design_name}.runs/impl_1/${design_name_wrapper}.sysdef ./${design_name}/${design_name}.sdk/${design_name}_wrapper.hdf
 
 #launch_sdk -workspace ./$design_name/zedboard_base.sdk -hwspec ./$design_name/zedboard_base.sdk/zedboard_base_wrapper.hdf
 
--- Vivado/sdk.tcl
+++ Vivado/sdk.tcl
@@ -5,15 +5,10 @@ if { ![info exists env(VIVADO_DESIGN_NAME)] } {
 set design_name $::env(VIVADO_DESIGN_NAME)
 puts "Using design name: ${design_name}"
 
-#file mkdir ./$design_name/$design_name.sdk
-#file copy -force ./$design_name/zedboard_base.runs/impl_1/zedboard_base_wrapper.sysdef ./$design_name/zedboard_base.sdk/zedboard_base_wrapper.hdf
-
-#launch_sdk -workspace ./$design_name/zedboard_base.sdk -hwspec ./$design_name/zedboard_base.sdk/zedboard_base_wrapper.hdf
-
 # Set SDK workspace
-setws ./$design_name/$design_name.sdk
+setws ./${design_name}/${design_name}.sdk
 # Create a HW project
-createhw -name hw1 -hwspec ./$design_name/$design_name.sdk/${design_name}_wrapper.hdf
+createhw -name hw1 -hwspec ./${design_name}/${design_name}.sdk/${design_name}_wrapper.hdf
 # Create a BSP project
 createbsp -name bsp1 -hwproject hw1 -proc ps7_cortexa9_0 -os standalone
 # Create application project
